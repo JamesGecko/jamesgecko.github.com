8603dc645db2ee313c88da1e90dedc42f4eb08e9
James D <jamesgecko@gmail.com>
2013-06-14 18:55:52 -0400

    Entire sections clickable
    Sections will also stay open if the user opened a link or selected text.
    Need to port things to zepto eventually.

--- resume/resume.js
+++ resume/resume.js
@@ -10,13 +10,21 @@ var toggleTriangle = function(selector) {
 
 $('.short').before('<div class="triangle">&#' + closedTriangleCode + '</div>');
 
-$('.short').on('click', function(e) {
-  toggleTriangle($(this).siblings('.triangle'));
-  $(this).siblings('.long').toggle();
-});
+var keepSectionOpen = function(e) { e.stopPropagation(); };
+$('.job a').on('click', keepSectionOpen);
 
-$('.long').on('click', function(e) {
-  toggleTriangle($(this).siblings('.triangle'));
-  $(this).toggle();
+// Keep the section open when the user has selected text.
+// http://stackoverflow.com/a/14711340/5625
+$('.job').on('mousedown', function(e) {
+  $(this).data('p0', { x: e.pageX, y: e.pageY });
+}).on('mouseup', function(e) {
+  var p0 = $(this).data('p0'),
+      p1 = { x: e.pageX, y: e.pageY },
+      d = Math.sqrt(Math.pow(p1.x - p0.x, 2) + Math.pow(p1.y - p0.y, 2));
+  if (d < 4) {
+    // Distance moved is small enough to be a click. Toggle the section.
+    toggleTriangle($(this).children('.triangle'));
+    var longClass = $(this).children('.long');
+    longClass.slideToggle(200);
+  }
 });
-
